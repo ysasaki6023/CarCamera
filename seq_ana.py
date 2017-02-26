e12f4a1fb8ead5ab2e60f5856ad352c12871a5cf
Yuichi Sasaki <ysasaki6023@gmail.com>
2017-02-26 15:00:53 +0900

    updated to adopt given image size

--- seq_ana.py
+++ seq_ana.py
@@ -38,29 +38,35 @@ def non_max_suppression_slow(boxes, overlapThresh, scoreThresh, sameClassOnly=Fa
     return boxes[np.logical_not(deleted)]
 
 
-def process_predicts(predicts):
+def process_predicts(predicts,common_params,net_params):
     p_classes = predicts[0, :, :, 0:20]
     C = predicts[0, :, :, 20:22]
     coordinate = predicts[0, :, :, 22:]
 
-    p_classes = np.reshape(p_classes, (7, 7, 1, 20))
-    C = np.reshape(C, (7, 7, 2, 1))
+    nx = net_params["cell_size_x"]
+    ny = net_params["cell_size_y"]
+    nb = net_params["boxes_per_cell"]
+    sizex = common_params["image_size_x"]
+    sizey = common_params["image_size_y"]
+
+    p_classes = np.reshape(p_classes, (ny,nx, 1, 20))
+    C = np.reshape(C, (ny,nx, nb, 1))
     P = C * p_classes
 
     def calc(k_index,k_P,k_coordinate):
         class_num = k_index[3]
-        k_coordinate = np.reshape(k_coordinate, (7, 7, 2, 4))
+        k_coordinate = np.reshape(k_coordinate, (ny,nx, nb, 4))
         max_coordinate = k_coordinate[index[0], index[1], index[2], :]
         xcenter = max_coordinate[0]
         ycenter = max_coordinate[1]
         w = max_coordinate[2]
         h = max_coordinate[3]
 
-        xcenter = (k_index[1] + xcenter) * (448/7.0)
-        ycenter = (k_index[0] + ycenter) * (448/7.0)
+        xcenter = (k_index[1] + xcenter) * (sizex/nx)
+        ycenter = (k_index[0] + ycenter) * (sizey/ny)
 
-        w = w * 448
-        h = h * 448
+        w = w * sizex
+        h = h * sizey
 
         xmin = xcenter - w/2.0
         ymin = ycenter - h/2.0
@@ -80,29 +86,33 @@ def process_predicts(predicts):
     return res
 
 
-def Run(iFiles, oFiles):
-    batchNum = 10
-    common_params = {'image_size': 448, 'num_classes': 20, 'batch_size':batchNum}
-    net_params = {'cell_size': 7, 'boxes_per_cell':2, 'weight_decay': 0.0005}
+def Run(iFiles, oFiles, img_size=[720,1280]):#height,width)
+    boxSize      = 64 # 448/7
+    h,w   = img_size
+    nh,nw = int(h/boxSize), int(w/boxSize)
+    hh,ww = nh*boxSize, nw*boxSize
+
+    batchNum = 1
+    common_params = {'image_size_x': ww, 'image_size_y': hh, 'num_classes': 20, 'batch_size':batchNum}
+    net_params = {'cell_size_x': nw, 'cell_size_y': nh, 'boxes_per_cell':2, 'weight_decay': 0.0005}
 
     net = YoloTinyNet(common_params, net_params, test=True)
 
-    image = tf.placeholder(tf.float32, (batchNum, 448, 448, 3))
+    image = tf.placeholder(tf.float32, (batchNum, hh, ww, 3))
     predicts = net.inference(image)
 
     sess = tf.Session()
 
     saver = tf.train.Saver(net.trainable_collection)
+    InitOp = tf.global_variables_initializer()
+    sess.run(InitOp)
     #saver.restore(sess,'models/pretrain/yolo_tiny.ckpt')
-    saver.restore(sess,'models/train_30penalty2000Iter/model.ckpt-2000')
-    #saver.restore(sess,'models/train/model.ckpt-5000')
-    #saver.restore(sess,'models/train/model.ckpt-1000')
-    #saver.restore(sess,'models/train/model.ckpt-5')
+    #saver.restore(sess,'models/train_30penalty2000Iter/model.ckpt-2000')
 
     currentPos = 0
 
     while currentPos<len(iFiles):
-        print(currentPos,len(iFiles))
+        if currentPos%10==0: print(currentPos)
         # Build batch file names
         batch_iFiles = []
         batch_oFiles = []
@@ -115,21 +125,20 @@ def Run(iFiles, oFiles):
             batch_oFiles = oFiles[currentPos:]
             num_files    = len(iFiles)-currentPos
 
-        resized_imgs = np.zeros((batchNum,448,448,3),dtype=np.float)
-        np_imgs      = np.zeros((batchNum,448,448,3),dtype=np.float)
+        resized_imgs = np.zeros((batchNum, hh, ww,3),dtype=np.float)
+        np_imgs      = np.zeros((batchNum, hh, ww,3),dtype=np.float)
         for fIndex, fName in enumerate(batch_iFiles):
             np_img = cv2.imread(fName)
-            h,w,_ = np_img.shape
-            size = min(h,w)
-            np_img = np_img[int(h/2-size/2):int(h/2+size/2),int(w/2-size/2):int(w/2+size/2)]
+            np_img = np_img[int(h/2-hh/2):int(h/2+hh/2),int(w/2-ww/2):int(w/2+ww/2)]
 
-            resized_img  = cv2.resize(np_img, (448, 448))
+            #resized_img  = cv2.resize(np_img, (448, 448))
+            resized_img = np_img
             np_img = cv2.cvtColor(resized_img, cv2.COLOR_BGR2RGB)
 
             np_img = np_img.astype(np.float32)
 
             np_img = np_img / 255.0 * 2 - 1
-            np_img = np.reshape(np_img, (1, 448, 448, 3))
+            np_img = np.reshape(np_img, (1, hh, ww, 3))
             np_imgs[fIndex] = np_img
             resized_imgs[fIndex] = resized_img
 
@@ -137,7 +146,7 @@ def Run(iFiles, oFiles):
 
         for index in range(num_files):
             np_predict = np.expand_dims(np_predict_list[index],0)
-            resList = process_predicts(np_predict)
+            resList = process_predicts(np_predict,common_params,net_params)
             #resList = np.expand_dims(resList[0],0)
             resList = non_max_suppression_slow(resList, overlapThresh=0.5, scoreThresh=0.05, sameClassOnly=False)
 
@@ -180,5 +189,5 @@ def RunVVC():
 
 if __name__=="__main__":
     #RunVideo1()
-    RunVideo(7)
+    RunVideo(6)
     #RunVVC()
